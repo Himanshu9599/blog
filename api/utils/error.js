d3178665cc71eaf427df2c8499cd3179b1b4e850
Himanshu9599 <himanshurawat9599@gmail.com>
2024-07-16 13:58:06 +0530

    add comments

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
 export const errorHandler = (statusCode, message) => {
-  const error = new Error();
-  error.statusCode = statusCode;
-  error.message = message;
-  return error;
+  const error = new Error();  // Create a new Error object
+  error.statusCode = statusCode;  // Set the statusCode property of the Error object
+  error.message = message;  // Set the message property of the Error object
+  return error;  // Return the Error object
 };
